8dafd5278221a12ffa6539ca2105ab67ef013f08
GeraGzGm <gerardo.gmagallon@alumnos.udg.mx>
2025-05-12 09:15:18 -0600

    Added base model for Kaggle datasets

--- ImageClassification/datasets/base_dataset.py
+++ ImageClassification/datasets/base_dataset.py
@@ -1,9 +1,14 @@
+import os
+import logging
 from enum import Enum, EnumMeta, Flag, EnumType
 from abc import ABC, abstractmethod, ABCMeta
 
 import numpy as np
 from torch import from_numpy, Tensor
 from torch.utils.data import Dataset, DataLoader
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+logger = logging.getLogger(__name__)
 
 class BaseDataset(ABC):
     """Abstract base class for all datasets"""
@@ -22,6 +27,55 @@ class BaseDataset(ABC):
     def get_classes(self) -> Enum:
         """Return the classes of the dataset."""
         pass
+
+class KaggleDataset:
+    """
+    Base class for the Kaggle Datasets.
+
+    Args:
+        dataset_name: Name of the user and dataset.
+        dataset_path: Path where the dataset will be stored.
+    
+    Example:
+        @DatasetRegistry.register("braincancer-mri")
+        class BrainCancer_MRI(KaggleDataset):
+            def __init__(self).
+                super().__init__("orvile/brain-cancer-mri-dataset", "./ImageClassification/braincaner-mri/")
+    
+    """
+    def __init__(self, dataset_name: str, dataset_path: str):
+        self.dataset_name = dataset_name
+        self.dataset_path = dataset_path
+
+        self.init_kaggle()
+        self.download_dataset()
+
+    def init_kaggle(self):
+        self.api = KaggleApi()
+        self.api.authenticate()
+
+    def download_dataset(self) -> None:
+        if not self._is_dataset_already_created():
+            logger.info(f"Downloading dataset {self.dataset_name} in: {self.dataset_path}")
+            self.api.dataset_download_files(self.dataset_name, path = self.dataset_path, unzip=True)
+
+        logger.info(f"Dataset {self.dataset_name} already downloaded at: {self.dataset_path}")
+
+    def _is_dataset_already_created(self) -> bool:
+        """
+        Check if the path already exists and if the path has files in order to define if the Dataset is already downloaded.
+
+        Returns:
+            bool: False if the dataset needs to be downloaded or True if it already exists.
+        """
+        if not os.path.exists(self.dataset_path):
+            return False
+        
+        if not any(os.scandir(self.dataset_path)):
+            return False
+        return True
+    
+
 
 class ABCEnumMeta(ABCMeta, EnumType):
     pass
